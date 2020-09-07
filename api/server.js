7ef41c7b396c92819c7305e6871bbbaf7d5b5149
Brandon Nichols <brandonaronnichols@gmail.com>
2020-09-06 23:02:10 -0500

    now commits should show

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const server = express();
 const produceRoute = require("../auth/plu/plu-router");
 
 // check to see if github commit working
+// changed email so that commits show
 server.use(express.json());
 server.use(cors());
 server.use("/api/produce", produceRoute);
