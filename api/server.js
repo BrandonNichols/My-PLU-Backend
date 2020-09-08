51e91f85b59fc9238fe14a9e83eaab93049e6eb4
Brandon Nichols <brandonaronnichols@gmail.com>
2020-09-07 19:54:03 -0500

    edited knexfile so that production enviornment configurations are set up

--- api/server.js
+++ api/server.js
@@ -3,8 +3,6 @@ const cors = require("cors");
 const server = express();
 const produceRoute = require("../auth/plu/plu-router");
 
-// check to see if github commit working
-// changed email so that commits show
 server.use(express.json());
 server.use(cors());
 server.use("/api/produce", produceRoute);
